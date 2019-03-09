9cd870e80114c37c412b67b62804b6de8eb9107f
Steve Wellington <steve@stevewellington.co.uk>
2019-03-09 00:43:48 +0000

    Start page and minimal user and big screen pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,94 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import firebase from "./firebase.js";
+
+const users = ["Liljana", "Cameron", "Matt", "Layne", "Steve", "Alan", "James"];
+
+const loginToApp = initials => {
+  const db = firebase.firestore();
+  return db
+    .collection("retros")
+    .doc("questions")
+    .update({
+      people: firebase.firestore.FieldValue.arrayUnion(initials)
+    });
+};
+
+const Start = ({ screenViewAction, loginAction }) => (
+  <div>
+    <div className="userlist">
+      <h1>Choose your name</h1>
+      {users.map(user => (
+        <button key={user} onClick={() => loginAction(user)}>
+          {user}
+        </button>
+      ))}
+    </div>
+    <button onClick={screenViewAction}>Screen view</button>
+  </div>
+);
+
+class ScreenView extends Component {
+  render() {
+    return <div>Hi</div>;
+  }
+}
+
+class UserView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hi {this.props.user}</div>;
+  }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "start"
+    };
+  }
+
+  goToScreenView = () => {
+    this.setState({
+      page: "screenview"
+    });
+  };
+
+  login = initials => {
+    loginToApp(initials).then(() =>
+      this.setState({
+        page: initials
+      })
     );
+  };
+
+  render() {
+    if (this.state.page === "start") {
+      return (
+        <div className="App">
+          <Start
+            screenViewAction={this.goToScreenView}
+            loginAction={this.login}
+          />
+        </div>
+      );
+    } else if (this.state.page === "screenview") {
+      return (
+        <div className="App">
+          <ScreenView />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <UserView user={this.state.page} />
+        </div>
+      );
+    }
   }
 }
 
