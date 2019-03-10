12e1b04d17b6c6ceab65bf99a20d944caf3f00cd
Steve Wellington <steve@stevewellington.co.uk>
2019-03-10 21:09:15 +0000

    Use carousel in user screens

--- src/UserView.js
+++ src/UserView.js
@@ -3,6 +3,7 @@ import firebase from "./firebase";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
 import FontAwesome from "react-fontawesome";
+import Carousel from "react-bootstrap/Carousel";
 
 class UserView extends Component {
   constructor(props) {
@@ -68,13 +69,11 @@ class QuestionView extends Component {
 
   render = () => {
     const {
-      questionState: { currentQuestion, questions },
+      questionState: { currentQuestion, currentScrollDirection, questions },
       user
     } = this.props;
 
-    const question = questions[currentQuestion];
-
-    const voteAction = score => {
+    const voteAction = (score, questionIndex) => {
       this.db
         .collection("retros")
         .doc("questions")
@@ -82,52 +81,63 @@ class QuestionView extends Component {
           votes: firebase.firestore.FieldValue.arrayUnion({
             user: user,
             score: score,
-            question: currentQuestion
+            question: questionIndex
           })
         });
       // TODO: disable buttons while persisting vote?
     };
 
-    const myVotes = this.state.votes.filter(
-      vote => vote.user === user && vote.question === currentQuestion
-    );
-
-    const VoteButton = ({ buttonScore }) => (
-      <Button
-        size="lg"
-        block
-        disabled={myVotes.length > 0}
-        onClick={() => voteAction(buttonScore)}
-      >
-        {buttonScore}
-        {myVotes.length > 0 && myVotes[0].score === buttonScore ? (
-          <FontAwesome name="check" />
-        ) : null}
-      </Button>
-    );
-
     return (
-      <div>
-        <Jumbotron style={{ background: "#" + question.colour }}>
-          <h1>{question.heading}</h1>
-          <h2>{question.subheading}</h2>
-          <p>
-            {question.examples.map((example, index) => (
-              <span key={index}>
-                <i>{example}</i>
-                <br />
-              </span>
-            ))}
-          </p>
-        </Jumbotron>
-        <div>
-          <VoteButton buttonScore={1} />
-          <VoteButton buttonScore={2} />
-          <VoteButton buttonScore={3} />
-          <VoteButton buttonScore={4} />
-          <VoteButton buttonScore={5} />
-        </div>
-      </div>
+      <Carousel
+        activeIndex={currentQuestion}
+        direction={currentScrollDirection}
+        interval={null}
+        controls={false}
+        indicators={false}
+      >
+        {questions.map((question, index) => {
+          const myVotes = this.state.votes.filter(
+            vote => vote.user === user && vote.question === index
+          );
+
+          const VoteButton = ({ buttonScore }) => (
+            <Button
+              size="lg"
+              block
+              disabled={myVotes.length > 0}
+              onClick={() => voteAction(buttonScore, index)}
+            >
+              {buttonScore}
+              {myVotes.length > 0 && myVotes[0].score === buttonScore ? (
+                <FontAwesome name="check" />
+              ) : null}
+            </Button>
+          );
+          return (
+            <Carousel.Item key={index}>
+              <Jumbotron style={{ background: "#" + question.colour }}>
+                <h1>{question.heading}</h1>
+                <h2>{question.subheading}</h2>
+                <p>
+                  {question.examples.map((example, index) => (
+                    <span key={index}>
+                      <i>{example}</i>
+                      <br />
+                    </span>
+                  ))}
+                </p>
+              </Jumbotron>
+              <div>
+                <VoteButton buttonScore={1} />
+                <VoteButton buttonScore={2} />
+                <VoteButton buttonScore={3} />
+                <VoteButton buttonScore={4} />
+                <VoteButton buttonScore={5} />
+              </div>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
     );
   };
 }
