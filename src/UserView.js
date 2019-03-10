80ab373a7a371d555e2d8cdee6512e1e45a88023
Steve Wellington <steve@stevewellington.co.uk>
2019-03-10 02:24:57 +0000

    Record votes

--- src/UserView.js
+++ src/UserView.js
@@ -28,7 +28,7 @@ class UserView extends Component {
         peopleCount={this.state.people.length}
       />
     ) : (
-      <QuestionView questionState={this.state} />
+      <QuestionView questionState={this.state} user={this.props.user} />
     );
 }
 
@@ -42,23 +42,136 @@ const UserWaitingScreen = ({ user, peopleCount }) => (
   </div>
 );
 
-const QuestionView = ({ questionState: { currentQuestion, questions } }) => {
-  const question = questions[currentQuestion];
-  return (
-    <div>
-      <div style={{ background: "#" + question.colour }}>
-        <h1>
-          Question {currentQuestion + 1}: {question.heading} -{" "}
-          {question.subheading}
-        </h1>
-        {question.examples.map((example, index) => (
-          <h3 key={index}>
-            <i>{example}</i>
-          </h3>
-        ))}
+class QuestionView extends Component {
+  constructor(props) {
+    super(props);
+    this.db = firebase.firestore();
+    this.state = {
+      votes: []
+    };
+  }
+
+  componentDidMount = () => {
+    this.db
+      .collection("retros")
+      .doc("questions")
+      .onSnapshot(questions => {
+        this.setState({
+          votes:
+            questions.data().votes === undefined ? [] : questions.data().votes
+        });
+      });
+  };
+
+  render = () => {
+    const {
+      questionState: { currentQuestion, questions },
+      user
+    } = this.props;
+
+    const question = questions[currentQuestion];
+
+    const voteAction = score => {
+      this.db
+        .collection("retros")
+        .doc("questions")
+        .update({
+          votes: firebase.firestore.FieldValue.arrayUnion({
+            user: user,
+            score: score,
+            question: currentQuestion
+          })
+        });
+      // TODO: disable buttons while persisting vote?
+    };
+
+    const myVotes = this.state.votes.filter(
+      vote => vote.user === user && vote.question === currentQuestion
+    );
+
+    return (
+      <div>
+        <div style={{ background: "#" + question.colour }}>
+          <h1>
+            Question {currentQuestion + 1}: {question.heading} -{" "}
+            {question.subheading}
+          </h1>
+          {question.examples.map((example, index) => (
+            <h3 key={index}>
+              <i>{example}</i>
+            </h3>
+          ))}
+        </div>
+        <div>
+          <button
+            disabled={myVotes.length > 0}
+            onClick={() => voteAction(1)}
+            className="voteButton"
+            style={{
+              border:
+                myVotes.length > 0 && myVotes[0].score === 1
+                  ? "2px solid red"
+                  : "1px solid black"
+            }}
+          >
+            1
+          </button>
+          <button
+            disabled={myVotes.length > 0}
+            onClick={() => voteAction(2)}
+            className="voteButton"
+            style={{
+              border:
+                myVotes.length > 0 && myVotes[0].score === 2
+                  ? "2px solid red"
+                  : "1px solid black"
+            }}
+          >
+            2
+          </button>
+          <button
+            disabled={myVotes.length > 0}
+            onClick={() => voteAction(3)}
+            className="voteButton"
+            style={{
+              border:
+                myVotes.length > 0 && myVotes[0].score === 3
+                  ? "2px solid red"
+                  : "1px solid black"
+            }}
+          >
+            3
+          </button>
+          <button
+            disabled={myVotes.length > 0}
+            onClick={() => voteAction(4)}
+            className="voteButton"
+            style={{
+              border:
+                myVotes.length > 0 && myVotes[0].score === 4
+                  ? "2px solid red"
+                  : "1px solid black"
+            }}
+          >
+            4
+          </button>
+          <button
+            disabled={myVotes.length > 0}
+            onClick={() => voteAction(5)}
+            className="voteButton"
+            style={{
+              border:
+                myVotes.length > 0 && myVotes[0].score === 5
+                  ? "2px solid red"
+                  : "1px solid black"
+            }}
+          >
+            5
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default UserView;
