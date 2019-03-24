902078a039b19ffa39f71b27cf46c51aefdf19cd
Steve Wellington <steve@stevewellington.co.uk>
2019-03-24 21:27:30 +0000

    Add script to bump major version

---
+++ bumpMajorVersion.bat
@@ -0,0 +1,8 @@
+cmd /c "npm version major"
+FOR /F "tokens=* USEBACKQ" %%F IN (`findstr "version" "package.json"`) DO (
+SET vers=%%F
+)
+set vers=%vers:"version": "=%
+set vers=%vers:",=%
+git add -A
+git commit -m "Bump version to %vers%"
