adc84a5e43ab75f45a3b7c2b699ce3a05f6d5a73
Steve Wellington <steve@stevewellington.co.uk>
2019-03-24 16:59:19 +0000

    Display more QR codes and URLs

--- src/LocationCode.js
+++ src/LocationCode.js
@@ -6,13 +6,26 @@ const LocationCode = () => {
   const url = window.location.href.replace(window.location.search, "");
   return (
     <Card style={{ textAlign: "center" }}>
-      <Card.Body>
-        <QRCode size={300} value={url} level={"H"} />
-        <br />
-        <h1>{url}</h1>
-        {url === "https://retro.stevewellington.dev/" ? (
+      <Card.Body style={{ display: "flex" }}>
+        <div style={{ flex: "0 0 50%" }}>
+          <QRCode
+            style={{ marginRight: "50px" }}
+            size={300}
+            value={url}
+            level={"H"}
+          />
+          <br />
+          <h1>{url}</h1>
+        </div>
+        <div style={{ flex: 1 }}>
+          <QRCode
+            size={300}
+            value="https://tinyurl.com/retro2019"
+            level={"H"}
+          />
+          <br />
           <h1>https://tinyurl.com/retro2019</h1>
-        ) : null}
+        </div>
       </Card.Body>
     </Card>
   );
